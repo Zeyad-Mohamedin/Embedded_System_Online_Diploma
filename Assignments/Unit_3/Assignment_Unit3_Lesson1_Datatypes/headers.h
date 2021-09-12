e2c8788890b0dbc60a8d65f610f31b35fb51f6e3
Zeyad Mohamedin <ziad.hassan1996@gmail.com>
2021-09-12 14:04:24 +0200

    assignment_Unit3_Lesson1

---
+++ Assignments/Unit_3/Assignment_Unit3_Lesson1_Datatypes/headers.h
@@ -0,0 +1,11 @@
+
+/* Author: Zeyad Mohamedin */
+/* Learn_In_Depth*/
+/*Assignment_Unit3_Lesson1*/
+
+typedef signed char int8_t;
+typedef unsigned char uint8_t;
+typedef short int16_t;
+typedef unsigned short uint16_t;
+typedef unsigned uint32_t;
+typedef long long int64_t;
